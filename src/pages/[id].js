7b5b2edf7365db2972aff0aa42f61bad16e2c1c1
Brad Cornes <bradlc41@gmail.com>
2020-09-16 16:56:42 +0100

    add share test

---
+++ src/pages/[id].js
@@ -0,0 +1,53 @@
+import Error from 'next/error'
+
+export default function Pen({ errorCode, initialContent }) {
+  if (errorCode) {
+    return <Error statusCode={errorCode} />
+  }
+
+  return initialContent.html
+}
+
+export async function getServerSideProps({ params }) {
+  const AWS = require('aws-sdk')
+
+  const db = new AWS.DynamoDB.DocumentClient({
+    credentials: {
+      accessKeyId: process.env.TW_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.TW_AWS_SECRET_ACCESS_KEY,
+    },
+    region: process.env.TW_AWS_DEFAULT_REGION,
+  })
+
+  function get(ID) {
+    return new Promise((resolve, reject) => {
+      const start = +new Date()
+      db.get(
+        { TableName: process.env.TW_TABLE_NAME, Key: { ID } },
+        (err, data) => {
+          if (err) reject(err)
+          console.log((+new Date() - start) / 1000)
+          resolve(data)
+        }
+      )
+    })
+  }
+
+  try {
+    const { Item: initialContent } = await get(params.id)
+
+    return {
+      props: initialContent
+        ? { initialContent }
+        : {
+            errorCode: 404,
+          },
+    }
+  } catch (_) {
+    return {
+      props: {
+        errorCode: 500,
+      },
+    }
+  }
+}
---
+++ src/pages/api/share.js
@@ -0,0 +1,59 @@
+import * as AWS from 'aws-sdk'
+import shortid from 'shortid'
+
+const db = new AWS.DynamoDB.DocumentClient({
+  credentials: {
+    accessKeyId: process.env.TW_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.TW_AWS_SECRET_ACCESS_KEY,
+  },
+  region: process.env.TW_AWS_DEFAULT_REGION,
+})
+
+function put(item) {
+  return new Promise((resolve, reject) => {
+    db.put(
+      {
+        TableName: process.env.TW_TABLE_NAME,
+        Item: item,
+      },
+      (err, data) => {
+        if (err) reject(err)
+        resolve(data)
+      }
+    )
+  })
+}
+
+export default async function share(req, res) {
+  if (req.method !== 'POST') {
+    res.statusCode = 404
+    return res.end()
+  }
+
+  if (
+    typeof req.body !== 'object' ||
+    typeof req.body.html !== 'string' ||
+    typeof req.body.css !== 'string' ||
+    typeof req.body.config !== 'string'
+  ) {
+    res.statusCode = 400
+    return res.end()
+  }
+
+  const ID = shortid.generate()
+
+  try {
+    await put({
+      ID,
+      html: req.body.html,
+      css: req.body.css,
+      config: req.body.config,
+    })
+    res.statusCode = 200
+    res.json({ ID })
+  } catch (err) {
+    console.log(err)
+    res.statusCode = 500
+    res.json({ error: true })
+  }
+}
