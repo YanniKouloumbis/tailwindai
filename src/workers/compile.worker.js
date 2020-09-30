5032a26f97e0e1f3ff049b0a424e95310f7e519b
Brad Cornes <bradlc41@gmail.com>
2020-09-30 18:47:32 +0100

    rework config require for browser compat and better errors

--- src/workers/compile.worker.js
+++ src/workers/compile.worker.js
@@ -3,19 +3,6 @@ import tailwindcss from 'tailwindcss'
 import resolveConfig from 'tailwindcss/resolveConfig'
 import extractClasses from './extractClasses'
 import { removeFunctions } from '../utils/object'
-///////////////
-import {
-  baseUrl as pageBaseUrl,
-  resolveImportMap,
-  createBlob,
-  resolveUrl,
-  resolveAndComposeImportMap,
-  hasDocument,
-  resolveIfNotPlainOrUrl,
-  dynamicImport,
-  resolvedPromise,
-} from './es-module-shims/common.js'
-import { init, parse } from 'es-module-lexer/dist/lexer.js'
 const applyComplexClasses = require('tailwindcss/lib/flagged/applyComplexClasses')
 
 // TODO
@@ -50,27 +37,69 @@ addEventListener('message', async (event) => {
 
   let mod = {}
 
-  const before = `(async function(module, self){
-    const require = async (m) => {
-      if (typeof m !== 'string' || m === '') throw Error('No module')
-      const result = await self.importShim('https://cdn.skypack.dev/' + m)
+  try {
+    await (0, eval)('import("")')
+  } catch (error) {
+    if (error instanceof TypeError) {
+      self.importShim = (0, eval)('u=>import(u)')
+    } else {
+      importScripts('https://unpkg.com/shimport@2.0.4/index.js')
+      self.importShim = __shimport__.load
+    }
+  }
+
+  class RequireError extends Error {
+    constructor(message, line) {
+      super(message)
+      this.name = 'RequireError'
+      this.line = line
+    }
+  }
+
+  const before = `(async function(module){
+    const require = async (m, line) => {
+      if (typeof m !== 'string') {
+        throw new RequireError('The "id" argument must be of type string. Received ' + typeof m, line)
+      }
+      if (m === '') {
+        throw new RequireError("The argument 'id' must be a non-empty string. Received ''", line)
+      }
+      let result
+      try {
+        result = await self.importShim('https://cdn.skypack.dev/' + m)
+      } catch (error) {
+        throw new RequireError("Cannot find module '" + m + "'", line)
+      }
       return result.default || result
     }`
-  const after = `})(mod, {})`
+  const after = `})(mod)`
 
   try {
     await eval(
       before +
         '\n' +
-        event.data.config.replace(/\brequire\(/g, 'await require(') +
+        event.data.config
+          .split('\n')
+          .map((line, i) =>
+            line.replace(
+              /\brequire\(([^(]*)\)/g,
+              (_m, id) =>
+                `await require(${id.trim() === '' ? 'undefined' : id}, ${
+                  i + 1
+                })`
+            )
+          )
+          .join('\n') +
         '\n' +
         after
     )
   } catch (error) {
     let line
 
-    if (typeof error.line !== 'undefined') {
-      line = error.line - 1
+    if (error instanceof RequireError) {
+      line = error.line
+    } else if (typeof error.line !== 'undefined') {
+      line = error.line - 1 - before.split('\n').length
     } else {
       const lines = error.stack.split('\n')
       for (let i = 0; i < lines.length; i++) {
@@ -81,7 +110,9 @@ addEventListener('message', async (event) => {
           matches.push(match)
         }
         if (matches.length > 0) {
-          line = parseInt(matches[matches.length - 1][1], 10)
+          line =
+            parseInt(matches[matches.length - 1][1], 10) -
+            before.split('\n').length
           break
         }
       }
@@ -91,10 +122,7 @@ addEventListener('message', async (event) => {
       error: {
         message: error.message,
         file: 'Config',
-        line:
-          typeof line === 'undefined'
-            ? undefined
-            : line - before.split('\n').length,
+        line: typeof line === 'undefined' ? undefined : line,
       },
     })
   }
@@ -148,267 +176,3 @@ addEventListener('message', async (event) => {
     }
   }
 })
-
-////////////////////////////////////////////////////
-///////////////////////////////////////////////////
-// https://github.com/guybedford/es-module-shims/blob/master/src/es-module-shims.js
-
-let id = 0
-const registry = {}
-
-async function loadAll(load, seen) {
-  if (load.b || seen[load.u]) return
-  seen[load.u] = 1
-  await load.L
-  return Promise.all(load.d.map((dep) => loadAll(dep, seen)))
-}
-
-let waitingForImportMapsInterval
-let firstTopLevelProcess = true
-async function topLevelLoad(url, source) {
-  if (waitingForImportMapsInterval > 0) {
-    clearTimeout(waitingForImportMapsInterval)
-    waitingForImportMapsInterval = 0
-  }
-  await importMapPromise
-  await init
-  const load = getOrCreateLoad(url, source)
-  const seen = {}
-  await loadAll(load, seen)
-  lastLoad = undefined
-  resolveDeps(load, seen)
-  const module = await dynamicImport(load.b)
-  // if the top-level load is a shell, run its update function
-  if (load.s) (await dynamicImport(load.s)).u$_(module)
-  return module
-}
-
-async function importShim(id, parentUrl) {
-  return topLevelLoad(resolve(id, parentUrl || pageBaseUrl))
-}
-
-self.importShim = importShim
-
-const meta = {}
-
-const edge = navigator.userAgent.match(/Edge\/\d\d\.\d+$/)
-
-async function importMetaResolve(id, parentUrl = this.url) {
-  await importMapPromise
-  return resolve(id, `${parentUrl}`)
-}
-
-Object.defineProperties(importShim, {
-  m: { value: meta },
-  l: { value: undefined, writable: true },
-  e: { value: undefined, writable: true },
-})
-importShim.fetch = (url) => fetch(url)
-importShim.skip = /^https?:\/\/(cdn\.pika\.dev|dev\.jspm\.io|jspm\.dev)\//
-// importShim.load = processScripts;
-
-let lastLoad
-function resolveDeps(load, seen) {
-  if (load.b || !seen[load.u]) return
-  seen[load.u] = 0
-
-  for (const dep of load.d) resolveDeps(dep, seen)
-
-  // "execution"
-  const source = load.S
-  // edge doesnt execute sibling in order, so we fix this up by ensuring all previous executions are explicit dependencies
-  let resolvedSource = edge && lastLoad ? `import '${lastLoad}';` : ''
-
-  const [imports] = load.a
-
-  if (!imports.length) {
-    resolvedSource += source
-  } else {
-    // once all deps have loaded we can inline the dependency resolution blobs
-    // and define this blob
-    let lastIndex = 0,
-      depIndex = 0
-    for (const { s: start, e: end, d: dynamicImportIndex } of imports) {
-      // dependency source replacements
-      if (dynamicImportIndex === -1) {
-        const depLoad = load.d[depIndex++]
-        let blobUrl = depLoad.b
-        if (!blobUrl) {
-          // circular shell creation
-          if (!(blobUrl = depLoad.s)) {
-            blobUrl = depLoad.s = createBlob(
-              `export function u$_(m){${depLoad.a[1]
-                .map((name) =>
-                  name === 'default'
-                    ? `$_default=m.default`
-                    : `${name}=m.${name}`
-                )
-                .join(',')}}${depLoad.a[1]
-                .map((name) =>
-                  name === 'default'
-                    ? `let $_default;export{$_default as default}`
-                    : `export let ${name}`
-                )
-                .join(';')}\n//# sourceURL=${depLoad.r}?cycle`
-            )
-          }
-        }
-        // circular shell execution
-        else if (depLoad.s) {
-          resolvedSource +=
-            source.slice(lastIndex, start - 1) +
-            '/*' +
-            source.slice(start - 1, end + 1) +
-            '*/' +
-            source.slice(start - 1, start) +
-            blobUrl +
-            source[end] +
-            `;import*as m$_${depIndex} from'${depLoad.b}';import{u$_ as u$_${depIndex}}from'${depLoad.s}';u$_${depIndex}(m$_${depIndex})`
-          lastIndex = end + 1
-          depLoad.s = undefined
-          continue
-        }
-        resolvedSource +=
-          source.slice(lastIndex, start - 1) +
-          '/*' +
-          source.slice(start - 1, end + 1) +
-          '*/' +
-          source.slice(start - 1, start) +
-          blobUrl
-        lastIndex = end
-      }
-      // import.meta
-      else if (dynamicImportIndex === -2) {
-        meta[load.r] = { url: load.r, resolve: importMetaResolve }
-        resolvedSource +=
-          source.slice(lastIndex, start) +
-          'importShim.m[' +
-          JSON.stringify(load.r) +
-          ']'
-        lastIndex = end
-      }
-      // dynamic import
-      else {
-        resolvedSource +=
-          source.slice(lastIndex, dynamicImportIndex + 6) +
-          'Shim(' +
-          source.slice(start, end) +
-          ', ' +
-          JSON.stringify(load.r)
-        lastIndex = end
-      }
-    }
-
-    resolvedSource += source.slice(lastIndex)
-  }
-
-  let sourceMappingResolved = ''
-  const sourceMappingIndex = resolvedSource.lastIndexOf('//# sourceMappingURL=')
-  if (sourceMappingIndex > -1) {
-    const sourceMappingEnd = resolvedSource.indexOf('\n', sourceMappingIndex)
-    const sourceMapping = resolvedSource.slice(
-      sourceMappingIndex,
-      sourceMappingEnd > -1 ? sourceMappingEnd : undefined
-    )
-    sourceMappingResolved =
-      `\n//# sourceMappingURL=` + resolveUrl(sourceMapping.slice(21), load.r)
-  }
-  load.b = lastLoad = createBlob(
-    resolvedSource + sourceMappingResolved + '\n//# sourceURL=' + load.r
-  )
-  load.S = undefined
-}
-
-function getOrCreateLoad(url, source) {
-  let load = registry[url]
-  if (load) return load
-
-  load = registry[url] = {
-    // url
-    u: url,
-    // response url
-    r: undefined,
-    // fetchPromise
-    f: undefined,
-    // source
-    S: undefined,
-    // linkPromise
-    L: undefined,
-    // analysis
-    a: undefined,
-    // deps
-    d: undefined,
-    // blobUrl
-    b: undefined,
-    // shellUrl
-    s: undefined,
-  }
-
-  const depcache = importMap.depcache[url]
-  if (depcache)
-    depcache.forEach((depUrl) => getOrCreateLoad(resolve(depUrl, url)))
-
-  load.f = (async () => {
-    if (!source) {
-      const res = await importShim.fetch(url)
-      if (!res.ok) throw new Error(`${res.status} ${res.statusText} ${res.url}`)
-      load.r = res.url
-      const contentType = res.headers.get('content-type')
-      if (contentType.match(/^(text|application)\/(x-)?javascript(;|$)/))
-        source = await res.text()
-      else throw new Error(`Unknown Content-Type "${contentType}"`)
-    }
-    try {
-      load.a = parse(source, load.u)
-    } catch (e) {
-      console.warn(e)
-      load.a = [[], []]
-    }
-    load.S = source
-    return load.a[0]
-      .filter((d) => d.d === -1)
-      .map((d) => source.slice(d.s, d.e))
-  })()
-
-  load.L = load.f.then(async (deps) => {
-    load.d = await Promise.all(
-      deps.map(async (depId) => {
-        let resolved = resolve(depId, load.r || load.u)
-        // https://cdn.skypack.dev/-/tailwindcss@v1.7.6-TG1T56mU2GyYfzeRbPoh/dist=es2020,mode=raw/resolveConfig
-        const match = resolved.match(
-          /https:\/\/cdn\.skypack\.dev\/-\/tailwindcss@.*?mode=raw\/(.*?)$/
-        )
-        if (match !== null) {
-          resolved = `/api/package?file=${match[1]}`
-        }
-        if (importShim.skip.test(resolved)) return { b: resolved }
-        const depLoad = getOrCreateLoad(resolved)
-        await depLoad.f
-        return depLoad
-      })
-    )
-  })
-
-  return load
-}
-
-const importMap = { imports: {}, scopes: {}, depcache: {} }
-let importMapPromise = resolvedPromise
-
-function resolve(id, parentUrl) {
-  return (
-    resolveImportMap(
-      importMap,
-      resolveIfNotPlainOrUrl(id, parentUrl) || id,
-      parentUrl
-    ) || throwUnresolved(id, parentUrl)
-  )
-}
-
-function throwUnresolved(id, parentUrl) {
-  throw Error(
-    "Unable to resolve specifier '" +
-      id +
-      (parentUrl ? "' from " + parentUrl : "'")
-  )
-}
