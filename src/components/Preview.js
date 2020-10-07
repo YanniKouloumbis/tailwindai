f3f91dc656d3e4d4686d96a6502c1482d4edce35
Brad Cornes <bradlc41@gmail.com>
2020-10-07 09:56:01 +0100

    hijack links in output

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -361,6 +361,25 @@ export const Preview = forwardRef(
                   </head>
                   <body style="display:none">
                   </body>
+                  <script>
+                  // https://github.com/sveltejs/svelte-repl/blob/master/src/Output/srcdoc/index.html
+                  // https://github.com/sveltejs/svelte-repl/blob/master/LICENSE
+                  document.body.addEventListener('click', event => {
+                    if (event.which !== 1) return;
+                    if (event.metaKey || event.ctrlKey || event.shiftKey) return;
+                    if (event.defaultPrevented) return;
+
+                    // ensure target is a link
+                    let el = event.target;
+                    while (el && el.nodeName !== 'A') el = el.parentNode;
+                    if (!el || el.nodeName !== 'A') return;
+
+                    if (el.hasAttribute('download') || el.getAttribute('rel') === 'external' || el.target) return;
+
+                    event.preventDefault();
+                    window.open(el.href, '_blank');
+                  });
+                  </script>
                 </html>
               `}
             />
