61c47c18bc4856699e3cdc6f0231ae0076d3b84f
Brad Cornes <bradlc41@gmail.com>
2020-10-07 10:09:14 +0100

    flip horizontal layout icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -38,14 +38,15 @@ export function Header({
             onClick={() => onChangeLayout('horizontal')}
           >
             <rect
-              x="2.75"
-              y="4.75"
+              x="21.25"
+              y="19.25"
               width="18.5"
               height="14.5"
               rx="1.25"
+              transform="rotate(-180 21.25 19.25)"
               fill="none"
             />
-            <path d="M2.75 12.75h18.5V18c0 .69-.56 1.25-1.25 1.25H4c-.69 0-1.25-.56-1.25-1.25v-5.25z" />
+            <path d="M21.25 11.25H2.75V6c0-.69.56-1.25 1.25-1.25h16c.69 0 1.25.56 1.25 1.25v5.25z" />
           </HeaderButton>
           <HeaderButton
             isActive={layout === 'preview'}
