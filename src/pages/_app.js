a39d33ec2dbd42ddbe362014ca5df8a521fe66ec
Brad Cornes <bradlc41@gmail.com>
2020-10-07 13:24:49 +0100

    add meta

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,11 @@
 import '../css/main.css'
 import Head from 'next/head'
 
+const URL = 'https://play.tailwindcss.com'
+const TITLE = 'Tailwind Play'
+const DESCRIPTION =
+  "An advanced online playground for Tailwind CSS that lets you use all of Tailwind's build-time features directly in the browser."
+
 if (typeof window !== 'undefined') {
   require('../workers/subworkers')
 }
@@ -9,7 +14,39 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Tailwind Play</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00b4b6" />
+        <meta name="theme-color" content="#ffffff" />
+        <title>{TITLE}</title>
+        <meta content={DESCRIPTION} name="description" />
+        <meta property="og:url" content={URL} />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content={TITLE} />
+        <meta property="og:title" content={TITLE} />
+        <meta property="og:description" content={DESCRIPTION} />
+        <meta property="og:image" content={`${URL}/social-card.jpg`} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@tailwindlabs" />
+        <meta name="twitter:title" content={TITLE} />
+        <meta name="twitter:description" content={DESCRIPTION} />
+        <meta name="twitter:image" content={`${URL}/social-card.jpg`} />
       </Head>
       <Component {...pageProps} />
     </>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -28,12 +28,6 @@ export default class Document extends NextDocument {
                 .replace("'inlocal", "' in local"),
             }}
           />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00b4b6" />
-          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body className="fixed overflow-hidden w-full min-h-full flex text-gray-900 dark:text-white bg-white dark:bg-gray-900">
           <Main />
